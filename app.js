b211dd20f5192b1f5fbcb719f9af5d968c81fed4
Jerry Lee Melton <jerryleemelton@gmail.com>
2022-02-22 16:48:27 -0800

    Add start of CoinGecko API code to app.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const coinGecko = require("coingecko-api");
 
 const app = express();
+const coinGeckoClient = new coinGecko();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -12,8 +14,9 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-
-
+app.get("/api", (req, res) => {
+  res.render("home");
+});
 
 
 
@@ -21,4 +24,21 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
   console.log("Server started on port 3000.");
+  getPrices();
 });
+
+
+var getPrices = async() => {
+  const params = {
+    vs_currency: "usd",
+    per_page: 25,
+    order: coinGecko.ORDER.MARKET_CAP_DESC,
+    price_change_percentage: "24h"
+  };
+
+  const result = await coinGeckoClient.coins.markets({params});
+
+  console.log(result);
+
+  return result;
+};
