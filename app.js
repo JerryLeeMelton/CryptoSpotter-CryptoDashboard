654daed899856f1a6e1e71cbbc56289c612dd130
Jerry Lee Melton <jerryleemelton@gmail.com>
2022-02-23 09:47:14 -0800

    Large commit.  Add fetching of data from CoinGecko API.  Format data from API.  Push data to a table in the pricing div.

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const coinGecko = require("coingecko-api");
+const https = require("https");
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD"
+});
 
 const app = express();
 const coinGeckoClient = new coinGecko();
@@ -9,9 +17,28 @@ const coinGeckoClient = new coinGecko();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+const api_url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h";
 
-app.get("/", (req, res) => {
-  res.render("home");
+
+app.get("/", async (req, res) => {
+  const fetch_response = await fetch(api_url);
+  const json = await fetch_response.json();
+  let priceData = [];
+
+  for (var i = 0; i < json.length; i++) {
+    priceData.push({
+      image: json[i].image,
+      id: json[i].id.charAt(0).toUpperCase() + json[i].id.slice(1),
+      symbol: json[i].symbol.toUpperCase(),
+      current_price: formatter.format(json[i].current_price),
+      price_change_percentage_24h_in_currency: parseFloat(json[i].price_change_percentage_24h_in_currency).toFixed(2) + "%",
+      market_cap: formatter.format(json[i].market_cap)
+    });
+  }
+
+  res.render("home", {
+    priceData: priceData
+  });
 });
 
 app.get("/api", (req, res) => {
@@ -24,21 +51,4 @@ app.get("/api", (req, res) => {
 
 app.listen(3000, () => {
   console.log("Server started on port 3000.");
-  getPrices();
 });
-
-
-var getPrices = async() => {
-  const params = {
-    vs_currency: "usd",
-    per_page: 25,
-    order: coinGecko.ORDER.MARKET_CAP_DESC,
-    price_change_percentage: "24h"
-  };
-
-  const result = await coinGeckoClient.coins.markets({params});
-
-  console.log(result);
-
-  return result;
-};
